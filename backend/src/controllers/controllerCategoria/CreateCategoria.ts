c8a983877f7955fac20373efcb1939a02726d9f8
Juan Alejandro <juan.acv.06@gmail.com>
2025-05-14 17:26:10 -0300

    Validações dos metodos de Categoria

---
+++ backend/src/controllers/controllerCategoria/CreateCategoria.ts
@@ -0,0 +1,32 @@
+import { z } from "zod";
+import { validation } from "../../service/middleware/Validation";
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+
+
+interface IBodyProps{
+    nome: string;
+}
+
+export const createCategoraValidation = validation((getSchema) => ({
+    body: getSchema<IBodyProps>(z.object({
+            nome: z.string({
+            required_error: 'Campo obrigatório.',
+            invalid_type_error: 'Campo obrigatório'
+        }).nonempty("Campo obrigatório")
+        .min(3, "O nome precisa ter 3 no mínimo caracteres")
+        .max(100, "O nome não pode ultrapassar 100 caracteres.")
+        .regex(/^[a-zA-ZÀ-ÿ\s]+$/, "Apenas letras e espaços são permitidos")
+        .trim()
+    }))
+}))
+
+export const CreateCategoria = (req: Request<{}, {}, IBodyProps>, res: Response) => {
+    console.log(req.body);
+
+    res.status(StatusCodes.BAD_REQUEST).json({
+        message: 'Ainda não implementado.'
+    })
+
+    return
+}
--- backend/src/routes/Routes.ts
+++ backend/src/routes/Routes.ts
@@ -6,6 +6,9 @@ import { DeleteProduto, deleteProdutoValidation } from "../controllers/controlle
 import { GetProdutoById, getProdutoByIdValidation } from "../controllers/controllerProdutos/GetById";
 import { GetAllProdutos, getAllProdutosValidation } from "../controllers/controllerProdutos/GetAllProdutos";
 import { UpdateProdutoById, updateProdutoValidation } from "../controllers/controllerProdutos/UpdateProduto";
+import { createCategoraValidation, CreateCategoria } from "../controllers/controllerCategoria/CreateCategoria";
+import { GetAllCategorias } from "../controllers/controllerCategoria/GetAllCategoria";
+import { DeleteCategoria, deleteCategoriaValidation } from "../controllers/controllerCategoria/DeleteCategoria";
 
 const appRouter = Router();
 
@@ -15,10 +18,18 @@ const appRouter = Router();
 
 
 //Rotas Produto
-appRouter.post('/',createProdutoValidation, CreateProduto);
-appRouter.get('/', getAllProdutosValidation, GetAllProdutos);
-appRouter.get('/:id', getProdutoByIdValidation, GetProdutoById);
-appRouter.put('/:id', updateProdutoValidation, UpdateProdutoById);
-appRouter.delete('/:id', deleteProdutoValidation, DeleteProduto);
+//appRouter.post('/',createProdutoValidation, CreateProduto);
+//appRouter.get('/', getAllProdutosValidation, GetAllProdutos);
+//appRouter.get('/:id', getProdutoByIdValidation, GetProdutoById);
+//appRouter.put('/:id', updateProdutoValidation, UpdateProdutoById);
+//appRouter.delete('/:id', deleteProdutoValidation, DeleteProduto);
+
+
+//Rotas Categoria
+//appRouter.post('/', createCategoraValidation, CreateCategoria);
+//appRouter.get('/', GetAllCategorias);
+//appRouter.delete('/:id', deleteCategoriaValidation, DeleteCategoria);
+
+//Rotas fornecedor
 
 export {appRouter}
