c8f9a95913f5e81eb63db7092f93475c45209cc6
Juan Alejandro <juan.acv.06@gmail.com>
2025-05-14 12:39:28 -0300

    feat: Arquivos da controller de produtos.

--- backend/src/controllers/controllerFuncionario/UpdateFuncionario.ts
+++ backend/src/controllers/controllerFuncionario/UpdateFuncionario.ts
@@ -1,74 +1,63 @@
-import { z } from "zod";
-import { validation } from "../../service/middleware/Validation";
+import { z } from "zod"
+import { validation } from "../../service/middleware/Validation"
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 
-interface IParamProps {
-  id?: number;
+
+interface IParamProps{
+    id?: number
 }
 
-interface IBodyProps {
-  nome: string;
-  email: string;
-  senha: string;
+interface IBodyProps{
+    nome: string,
+    email: string,
+    senha: string
 }
 
 export const updateFuncionarioValidation = validation((getSchema) => ({
-  params: getSchema<IParamProps>(
-    z.object({
-      id: z.coerce
-        .number()
-        .positive("Deve ser maior que 0.")
-        .int("Deve ser um número inteiro.")
-        .optional(),
-    })
-  ),
-  body: getSchema<IBodyProps>(
-    z.object({
-      nome: z
-        .string({
-          required_error: "Campo obrigatório",
-          invalid_type_error: "Somente letras",
-        })
-        .nonempty("Campo obrigatório")
-        .min(3, "O nome precisa ter 3 no mínimo caracteres")
-        .max(100, "O nome não pode ultrapassar 100 caracteres.")
-        .regex(/^[a-zA-ZÀ-ÿ\s]+$/, "Apenas letras e espaços são permitidos")
-        .trim(),
-      email: z
-        .string({
-          required_error: "Campo obrigatório",
-        })
-        .email("Informe um e-mail válido.")
-        .nonempty("Campo obrigatório")
-        .min(13, "O email precisa ter no mínimo 13 caracteres")
-        .max(100, "O campo não pode ultrapassar 100 caracteres.")
-        .trim(),
-      senha: z
-        .string({
-          required_error: "Campo obrigatório",
-        })
-        .nonempty("Campo obrigatório")
-        .min(8, "A senha precisa ter no mínimo 8 caracteres")
-        .max(100, "O campo não pode ultrapassar 100 caracteres.")
-        .trim()
-        .regex(/[a-z]/, "A senha deve conter pelo menos uma letra minúscula")
-        .regex(/[A-Z]/, "A senha deve conter pelo menos uma letra maiúscula")
-        .regex(/[0-9]/, "A senha deve conter pelo menos um número")
-        .regex(/[\W_]/, "A senha deve conter pelo menos um caractere especial"),
-    })
-  ),
+    params: getSchema<IParamProps>(z.object({
+        id: z.coerce.number().positive('Deve ser maior que 0.').int('Deve ser um número inteiro.').optional()
+    })),
+    body: getSchema<IBodyProps>(z.object({
+        nome: z.string({
+                  required_error: "Campo obrigatório",
+                  invalid_type_error: "Somente letras",
+                })
+                .nonempty("Campo obrigatório")
+                .min(3, "O nome precisa ter 3 no mínimo caracteres")
+                .max(100, "O nome não pode ultrapassar 100 caracteres.")
+                .regex(/^[a-zA-ZÀ-ÿ\s]+$/, "Apenas letras e espaços são permitidos")
+                .trim(),
+        email: z
+                .string({
+                  required_error: "Campo obrigatório",
+                })
+                .email("Informe um e-mail válido.")
+                .nonempty("Campo obrigatório")
+                .min(13, "O email precisa ter no mínimo 13 caracteres")
+                .max(100, "O campo não pode ultrapassar 100 caracteres.")
+                .trim(),
+        senha: z
+                .string({
+                  required_error: "Campo obrigatório",
+                })
+                .nonempty("Campo obrigatório")
+                .min(8, "A senha precisa ter no mínimo 8 caracteres")
+                .max(100, "O campo não pode ultrapassar 100 caracteres.")
+                .trim()
+                .regex(/[a-z]/, "A senha deve conter pelo menos uma letra minúscula")
+                .regex(/[A-Z]/, "A senha deve conter pelo menos uma letra maiúscula")
+                .regex(/[0-9]/, "A senha deve conter pelo menos um número")
+                .regex(/[\W_]/, "A senha deve conter pelo menos um caractere especial")
+    }))
 }));
 
-export const UpdateFuncionarioById = async (
-  req: Request<IParamProps, {}, IBodyProps>,
-  res: Response
-) => {
-  console.log(req.params);
-  console.log(req.body);
+export const UpdateFuncionarioById = async(req: Request<IParamProps, {}, IBodyProps>, res: Response) => {
+    console.log(req.params);
+    console.log(req.body);
 
-  res.status(StatusCodes.BAD_REQUEST).json({
-    message: "Método ainda não implementado.",
-  });
-  return;
-};
+    res.status(StatusCodes.BAD_REQUEST).json({
+        message: 'Método ainda não implementado.'
+    })
+    return;
+}
