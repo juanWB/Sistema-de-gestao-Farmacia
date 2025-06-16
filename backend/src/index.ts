dd516a3bec49b1394aa3df808dc71e9a54f22ecf
Juan Alejandro <juan.acv.06@gmail.com>
2025-06-16 10:29:43 -0300

    feat: Ajustes para retorno do funcionamento em produção

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,15 @@
+import { Knex } from "./database/knex";
 import { app } from "./server/AppServer";
 
-app.listen(process.env.PORT || 3000, () => {
+const startServer = () => {app.listen(process.env.PORT || 3000, () => {
     console.log('Rodando na porta ' + process.env.PORT);
-})
+})}
+
+if(process.env.IS_LOCALHOST !== 'true'){
+    Knex.migrate.latest()
+        .then(() => {
+            startServer()
+        }).catch(console.log);
+}else{
+    startServer();
+}
