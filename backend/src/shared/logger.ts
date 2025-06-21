1baef542db95ebf9901f562e768ec0a0f5b938ef
Juan Alejandro <juan.acv.06@gmail.com>
2025-06-21 11:13:50 -0300

    feat: instalação do winston e implementação do logger com winston

---
+++ backend/src/shared/logger.ts
@@ -0,0 +1,17 @@
+import { createLogger, format, info, transports } from "winston";
+
+export const logger = createLogger({
+    level: 'info',
+    format: format.combine(
+        format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss'}),
+        format.printf(({level, message, timestamp}) => {
+            return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
+        })
+    ),
+    transports: [
+        new transports.Console(),
+        new transports.File({ filename: 'logs/error.log', level: 'error'}),
+        new transports.File({ filename: 'logs/combined.log'}),
+    ]
+
+})
