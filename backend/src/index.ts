e35757e109d45be550a4ecfcd9319dfcbde4780c
Juan Alejandro <juan.acv.06@gmail.com>
2025-06-26 14:37:08 -0300

    feat: ajuste no PORT

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,20 @@
 import { Knex } from "./database/knex";
 import { app } from "./server/AppServer";
 
-const startServer = () => {app.listen(process.env.PORT || 3000, () => {
-    console.log('Rodando na porta ' + process.env.PORT);
-})}
+const PORT = process.env.PORT || 3000;
+
+const startServer = () => {
+    app.listen(PORT, () => {
+        console.log('Rodando na porta ' + PORT);
+    })
+}
 
 if(process.env.IS_LOCALHOST !== 'true'){
-    Knex.migrate.latest()
-        .then(() => {
-            Knex.seed.run().then(() => {
-                 startServer();
-            }).catch(console.log);
+    Knex.migrate.latest().then(() => {
+        Knex.seed.run().then(() => {
+                startServer();
         }).catch(console.log);
+    }).catch(console.log);
 }else{
     startServer();
 }
