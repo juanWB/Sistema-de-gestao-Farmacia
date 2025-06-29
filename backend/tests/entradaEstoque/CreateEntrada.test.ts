fd1017e4d7e310a812c9ce8d970b771a5005654e
Juan Alejandro <juan.acv.06@gmail.com>
2025-06-29 15:13:29 -0300

    feat: melhoras nos teste unitarios de createEntrada e getAllEntrada

--- backend/tests/entradaEstoque/CreateEntrada.test.ts
+++ backend/tests/entradaEstoque/CreateEntrada.test.ts
@@ -3,28 +3,32 @@ import { serverTest } from "../jest.setup";
 
 describe("EntradaEstoqueController - Create", () => {
   describe("Criação válida", () => {
-    it("Cria uma entrada no estoque com parametros corretos.", async () => {
-       const response1 = await serverTest.post("/fornecedor").send({
-      nome: "Atacamax",
-      cnpj: "12.345.678/9123-45",
-      telefone: "(81) - 998837891",
-      endereco: "Rua Major",
+    let produto_id: number | undefined = undefined;
+    beforeAll(async () => {
+      const fonercedorId = await serverTest.post("/fornecedor").send({
+        nome: "Atacamax",
+        cnpj: "12.345.678/9123-45",
+        telefone: "(81) - 998837891",
+        endereco: "Rua Major",
+      });
+
+      const categoriaId = await serverTest.post("/categorias").send({ nome: "Medicamentos" });
+
+      const response = await serverTest.post("/produto").send({
+        nome: "Sabonete",
+        preco: "1.99",
+        validade: "2025-01-01",
+        quantidade: "100",
+        categoria_id: categoriaId,
+        fornecedor_id: fonercedorId,
+      });
+
+      produto_id = response.body;
     });
 
-    const response2 = await serverTest.post("/categorias").send({ nome: "Medicamentos" });
-
-    const response = await serverTest.post("/produto").send({
-      id: 1,
-      nome: "Sabonete",
-      preco: "1.99",
-      validade: "2025-01-01",
-      quantidade: "100",
-      categoria_id: 1,
-      fornecedor_id: 1,});
-
-
+    it("Cria uma entrada no estoque com parametros corretos.", async () => {
       const res = await serverTest.post("/entrada").send({
-        produto_id: 1,
+        produto_id,
         quantidade: 2,
         entrada_data: "2000-06-17",
       });
--- backend/tests/entradaEstoque/GetAllEntrada.test.ts
+++ backend/tests/entradaEstoque/GetAllEntrada.test.ts
@@ -2,26 +2,31 @@ import { StatusCodes } from "http-status-codes";
 import { serverTest } from "../jest.setup";
 
 describe("EntradaEstoqueController - GetAll", () => {
-  it("Busca todas entradas no estoque", async () => {
-    await serverTest.post("/fornecedor").send({
-      nome: "Atacamax",
-      cnpj: "12.345.678/9123-45",
-      telefone: "(81) - 998837891",
-      endereco: "Rua Major",
+  let produto_id: number | undefined = undefined;
+    beforeAll(async () => {
+      const fonercedorId = await serverTest.post("/fornecedor").send({
+        nome: "Atacamax",
+        cnpj: "12.345.678/9123-45",
+        telefone: "(81) - 998837891",
+        endereco: "Rua Major",
+      });
+
+      const categoriaId = await serverTest.post("/categorias").send({ nome: "Medicamentos" });
+
+      const response = await serverTest.post("/produto").send({
+        nome: "Sabonete",
+        preco: "1.99",
+        validade: "2025-01-01",
+        quantidade: "100",
+        categoria_id: categoriaId,
+        fornecedor_id: fonercedorId,
+      });
+
+      produto_id = response.body;
     });
-
-    await serverTest.post("/categorias").send({ nome: "Medicamentos" });
-
-    await serverTest.post("/produto").send({
-      nome: "Sabonete",
-      preco: "1.99",
-      validade: "2025-01-01",
-      quantidade: "100",
-      categoria_id: 1,
-      fornecedor_id: 1,});
-
+  it("Busca todas entradas no estoque", async () => {
     const res1 = await serverTest.post("/entrada").send({
-      produto_id: 1,
+      produto_id,
       quantidade: 2,
       entrada_data: "2000-06-17",
     });
