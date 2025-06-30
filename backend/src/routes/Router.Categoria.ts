470d69a505e7fcf216249e9224e0bd4cdcfcc3ab
Juan Alejandro <juan.acv.06@gmail.com>
2025-06-30 19:03:01 -0300

    feat: ajuste nas rotas de categoria para deploy

--- backend/src/routes/Router.Categoria.ts
+++ backend/src/routes/Router.Categoria.ts
@@ -3,9 +3,6 @@ import { controllerCategoria } from "../controllers/controllerCategoria";
 const routerCategoria = Router();
 
 //Rotas Categoria
-routerCategoria.post('/categorias',controllerCategoria.createCategoraValidation, controllerCategoria.CreateCategoria);
 routerCategoria.get('/categorias', controllerCategoria.getAllCategoriasValidation ,controllerCategoria.GetAllCategorias);
-routerCategoria.delete('/categorias/:id', controllerCategoria.deleteCategoriaValidation, controllerCategoria.DeleteCategoria);
-routerCategoria.put('/categorias/:id', controllerCategoria.updateCategoriaValidation, controllerCategoria.UpdateCategoria);
 
 export {routerCategoria}
