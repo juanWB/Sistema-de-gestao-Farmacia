57c62372fe61289bed344aeffa7ecae4eb560242
Juan Alejandro <juan.acv.06@gmail.com>
2025-06-30 19:04:57 -0300

    feat: ajuste na controllerCategoria

--- backend/src/controllers/controllerCategoria/index.ts
+++ backend/src/controllers/controllerCategoria/index.ts
@@ -1,11 +1,6 @@
-import * as create from './CreateCategoria';
 import * as getAll from './GetAllCategoria';
-import * as update from './UpdateCategoria';
-import * as remove from './DeleteCategoria';
+
 
 export const controllerCategoria = {
-    ...create,
     ...getAll,
-    ...update,
-    ...remove
 };
