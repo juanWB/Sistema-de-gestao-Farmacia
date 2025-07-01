75e13b3ae888729560a95dae26c45c326b24e7a7
Juan Alejandro <juan.acv.06@gmail.com>
2025-06-30 21:43:23 -0300

    feat: melhoria na provider e controller de saida estoque

--- backend/src/controllers/controllerSaidaEstoque/GetAllSaidaEstoque.ts
+++ backend/src/controllers/controllerSaidaEstoque/GetAllSaidaEstoque.ts
@@ -8,7 +8,7 @@ interface IQueryProps{
     page?: number,
     limit?: number,
     filter?: string,
-    id?: number
+    produto_id?: number
 }
 
 export const getAllSaidasValidation = validation((getSchema) => ({
@@ -16,13 +16,13 @@ export const getAllSaidasValidation = validation((getSchema) => ({
             page: z.coerce.number().positive('Deve ser maior que zero').int('Deve ser um inteiro').optional(),
             limit: z.coerce.number().positive('Deve ser maior que zero').int('Deve ser um inteiro').optional(),
             filter: z.string().optional(),
-            id: z.coerce.number().positive('Deve ser maior que zero').int('Deve ser um inteiro').optional()
+            produto_id: z.coerce.number().positive('Deve ser maior que zero').int('Deve ser um inteiro').optional()
     }))
 }))
 
 export const GetAllSaidas = async(req: Request<{}, {}, {}, IQueryProps>, res: Response) => {
     
-    const result = await SaidaProvider.GetSaidaProvider(req.query.page || 1, req.query.limit = 10, req.query.filter || '', req.query.id || 0);
+    const result = await SaidaProvider.getSaidaProvider(req.query.page || 1, req.query.limit = 10, req.query.filter || '', req.query.produto_id || 0);
     const count = await SaidaProvider.Count();
 
      if(result instanceof Error){
--- backend/src/database/providers/saidaEstoqueProviders/GetSaidaProvider.ts
+++ backend/src/database/providers/saidaEstoqueProviders/GetSaidaProvider.ts
@@ -1,34 +1,35 @@
 import { logger } from "../../../shared/logger";
 import { ETableNames } from "../../ETableNames";
 import { Knex } from "../../knex";
-import { IProduto } from "../../models";
+import { ISaidaEstoque } from "../../models";
 
-export const GetSaidaProvider = async (
+export const getSaidaProvider = async (
   page: number,
   limit: number,
   filter: string,
-  id = 0
-): Promise<IProduto[] | Error> => {
+  produto_id: number
+): Promise<ISaidaEstoque[] | Error> => {
   try {
     const result = await Knex(ETableNames.saidaEstoque)
       .select("*")
-      .where("id", id)
-      .orWhere("saida_data", "like", `%${filter}%`)
+      .modify((query) => {
+        if (filter) {
+          query.where("saida_data", "like", `%${filter}%`);
+        }
+
+        if (produto_id && produto_id > 0) {
+          query.andWhere("produto_id", produto_id);
+        }
+      })
       .offset((page - 1) * limit)
       .limit(limit);
-
-    if (id > 0 && result.every((item) => item.id !== id)) {
-      const resultById = await Knex(ETableNames.saidaEstoque)
-        .select("*")
-        .where("id", id).first;
-
-      if (resultById) return [...result, resultById];
-    }
 
     logger.info(`GetSaidaProvider executado com sucesso.`);
     return result;
   } catch (err) {
-    logger.error(`GetSaidaProvider falhou ao buscar vategorias: ${JSON.stringify(err)}`);
-    return new Error("Error ao buscar saidas do estoque");
+    logger.error(
+      `GetSaidaProvider falhou ao buscar vategorias: ${JSON.stringify(err)}`
+    );
+    return new Error("Error ao buscar entradas no estoque");
   }
 };
