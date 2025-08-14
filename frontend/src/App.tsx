8352518c803a80950e1089d2a79adcc92b16f8a8
Juan Alejandro <juan.acv.06@gmail.com>
2025-08-14 10:57:02 -0300

    feat: inserting authContext and login on App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,19 +2,28 @@ import React from 'react'
 import { AppRoutes } from './routes'
 import { MenuLateral } from './shared/components/index'
 import { BrowserRouter } from 'react-router-dom'
-import { AppThemeProvider, DrawerProvider } from './shared/contexts/index'
+import { AppThemeProvider, AuthContextProvider, DrawerProvider } from './shared/contexts/index'
+import { Login } from './shared/components/login/Login'
 export const App = () => {
 
   return (
-    <AppThemeProvider>
-      <DrawerProvider>
-        <BrowserRouter>
-          <MenuLateral>
-            <AppRoutes />
-          </MenuLateral>
-        </BrowserRouter>
-      </DrawerProvider>
-    </AppThemeProvider>
+    <AuthContextProvider>
+
+      <AppThemeProvider>
+        <Login>
+          <DrawerProvider>
+
+            <BrowserRouter>
+              <MenuLateral>
+                <AppRoutes />
+              </MenuLateral>
+            </BrowserRouter>
+
+          </DrawerProvider>
+        </Login>
+      </AppThemeProvider>
+
+    </AuthContextProvider >
   )
 }
 
