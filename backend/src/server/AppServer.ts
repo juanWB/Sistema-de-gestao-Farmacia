00aaf82397604843b08933e55437a222d4a025f5
Juan Alejandro <juan.acv.06@gmail.com>
2025-08-14 11:53:41 -0300

    feat: enabling cors to connect with the  frontend.

--- backend/src/server/AppServer.ts
+++ backend/src/server/AppServer.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import "dotenv/config";
-//import cors from 'cors';
+import cors from 'cors';
 import { appRouter } from "../routes/AppRoutes";
 
 const app = express();
 
-// app.use(cors({
-//     origin: process.env.ENABLE_CORS?.split('') || []
-// }))
+app.use(cors({
+     origin: process.env.ENABLE_CORS?.split('') || []
+}))
 
 app.use(express.json());
 app.use(appRouter);
