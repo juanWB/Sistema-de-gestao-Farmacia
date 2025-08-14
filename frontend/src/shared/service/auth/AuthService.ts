ad8fe437d3cb319668a604fa310a7ef5721f68cc
Juan Alejandro <juan.acv.06@gmail.com>
2025-08-14 08:54:07 -0300

    feat: register method created/auth srevice done

--- frontend/src/shared/service/auth/AuthService.ts
+++ frontend/src/shared/service/auth/AuthService.ts
@@ -13,14 +13,29 @@ const entrar = async(email: string, senha: string):Promise<IAuth | Error> => {
             return data;
         }
 
-        return new Error('Error no login');   
+        return new Error('Error na tentativa de login');   
     }catch(error){
         console.log(error);
-        return new Error((error as {message: string}).message + ' Error no login')
+        return new Error((error as {message: string}).message + ' Error na tentativa de login')
     }
-}   
+}  
+
+const cadastrar = async(nome: string, email: string, senha: string):Promise<number | Error> => {
+    try{
+        const { data } = await Api.post('/cadastrar', { nome, email, senha });
+        
+        if(data){
+            return data;
+        }
+        
+        return new Error('Error na tentativa de cadastro')
+    }catch(error){
+        console.log(error);
+        return new Error((error as {message: string}).message + 'Error na tentativa de cadastro');
+    }
+}
 
 export const AuthService = {
     entrar,
-
+    cadastrar
 }
