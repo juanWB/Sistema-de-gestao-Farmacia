af62a9f98c56752ba70de41a9ec1bc2abf42bff6
Juan Alejandro <juan.acv.06@gmail.com>
2025-08-16 14:15:15 -0300

    feat: update method from EntradaEstoqueController done

--- backend/src/controllers/controllerEntradaEstoque/UpdateEntradaEstoque.ts
+++ backend/src/controllers/controllerEntradaEstoque/UpdateEntradaEstoque.ts
@@ -2,14 +2,14 @@ import { z } from "zod";
 import { validation } from "../../shared/middleware/Validation";
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
-import { IFornecedor } from "../../database/models/index";
-import { FornecedorProvider } from "../../database/providers/fornecedorProviders";
+import { IEntradaEstoque } from "../../database/models/index";
+import { EntradaEstoqueProvider } from "../../database/providers/entradaEstoqueProviders";
 
 interface IParamProps {
   id?: number;
 }
 
-interface IBodyProps extends Omit<IFornecedor, 'id'>{};
+interface IBodyProps extends Omit<IEntradaEstoque, 'id'>{};
 
 export const updateFornecedorValidation = validation((getSchema) => ({
   params: getSchema<IParamProps>(
@@ -22,60 +22,30 @@ export const updateFornecedorValidation = validation((getSchema) => ({
         .int("Deve ser um inteiro"),
     })
   ),
-  body: getSchema<IBodyProps>(
-    z.object({
-      nome: z
-        .string({
-          required_error: "Campo obrigatório.",
-          invalid_type_error: "Campo obrigatório",
+body: getSchema<IBodyProps>(z.object({
+        produto_id: z.coerce.number({
+            required_error: "Campo obrigatório",
+            invalid_type_error: "O id precisar ser um número."
+        }).positive('Deve ser maior que 0.').int('O id deve ser um inteiro.'),
+        quantidade: z.coerce.number({
+            required_error: "",
+            invalid_type_error: "A quantidade precisar ser um número."
+        }).positive('Deve ser maior que 0.').int('A quantidade precisa ser um inteiro.'),
+       entrada_data: z.string({
+            required_error: 'Campo obrigatório.',
+            invalid_type_error: 'Campo obrigatório'
         })
-        .nonempty("Campo obrigatório")
-        .min(3, "O nome precisa ter 3 no mínimo caracteres")
-        .max(100, "O nome não pode ultrapassar 100 caracteres.")
-        .regex(/^[a-zA-ZÀ-ÿ\s]+$/, "Apenas letras e espaços são permitidos")
-        .trim(),
-      cnpj:  z
-        .string({
-          required_error: "Campo obrigatório.",
-          invalid_type_error: "Campo obrigatório",
-        })
-        .nonempty("Campo obrigatório")
-        .transform((val) => val.replace(/\D/g, ""))
-        .refine((val) => val.length === 14, {
-          message: "CNPJ inválido, precisa ter exatamente 14 dígitos",
-        }),
-      telefone:  z
-        .string({
-          required_error: "Campo obrigatório.",
-          invalid_type_error: "Campo obrigatório",
-        })
-        .nonempty("Campo obrigatório")
-        .transform((val) => val.replace(/[^\d]/g, ""))
-        .refine((val) => val.length >= 10 && val.length <= 15, {
-          message: "Telefone inválido, precisa ter enter 10 e 14 dígitos",
-        }),
-      endereco:z
-        .string({
-          required_error: "Campo obrigatório.",
-          invalid_type_error: "Campo obrigatório",
-        })
-        .nonempty("Campo obrigatório")
-        .min(3, "Precisa ter ao menos 3 caracteres.")
-        .regex(
-          /^[a-zA-ZÀ-ÿ\s]+$/,
-          "Rua inválida apenas letras e espaços são permitidos"
-        )
-        .trim(),
-    })
-  ),
+        .nonempty('Campo obrigatório').
+        regex(/^\d{2}-\d{2}-\d{4}$/, 'O formato deve ser DD-MM-YYYY')
+        .transform((str) => new Date(str))
+        .refine((date) => !isNaN(date.getTime()), { message: 'Data inválida' })
+        ,
+    }))
 }));
 
 export const updateFornecedor = async(req: Request<IParamProps, {}, IBodyProps>, res: Response) => {
-  const {id} = req.params;
-  const fornecedor = req.body;
-
   
-  if(!id){
+  if(!req.params.id){
       res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
       errors:{
           default: 'O id é um parametro obrigatório.'
@@ -84,7 +54,7 @@ export const updateFornecedor = async(req: Request<IParamProps, {}, IBodyProps>,
       return
   }
 
-  const result = await FornecedorProvider.updateFornecedorProvider(id!, fornecedor);
+  const result = await EntradaEstoqueProvider.updateEntradaEstoqueProvider(req.params.id!, req.body);
   
   if(result instanceof Error){
       res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
